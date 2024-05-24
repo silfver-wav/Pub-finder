7f8d848c4d7b93138b9098828cc3d87b02c43251
Linus Silfver <linus.silfver@gmail.com>
2024-05-24 12:01:53 +0200

    fixed the searchBar.jsx to clear after search and reduce api calls

--- src/searchBar/searchBar.jsx
+++ src/searchBar/searchBar.jsx
@@ -1,23 +1,29 @@
-import { React, useEffect, useState, useRef } from "react";
+import { React, useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Input } from "@material-tailwind/react";
 import { useSearchForPubQuery } from "../redux/slices/apiSlices/pubApiSlice";
 import { skipToken } from "@reduxjs/toolkit/query";
-import SearchResults from "./searchResults";
+import correctEncoding from "../utils/correctEncoding";
+import { setPub } from "../redux/slices/pubSlice";
+import { useGetPubQuery } from "../redux/slices/apiSlices/pubApiSlice";
+import stopWords from "../utils/stopWords";
+import { useGetPubsQuery } from "../redux/slices/apiSlices/pubApiSlice";
 
 export default function SearchBar() {
   const dispatch = useDispatch();
-  const stopWords = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for', 'if', 'in', 'into', 'is', 'it', 'no',
-    'not', 'of', 'on', 'or', 'such', 'that', 'the', 'their', 'then', 'there', 'these', 'they', 'this',
-    'to', 'was', 'will', 'with'
-  ];
+  const geocode = useSelector((state) => state.pubs.geocode);
+  const [inputValue, setInputValue] = useState('');
   const [term, setTerm] = useState();
   const { data: result = [] } = useSearchForPubQuery(term ? term : skipToken);
   const [searchResults, setSearchResults] = useState([])
+  const [pubId, setPubId] = useState(null);
+
+  const { data: pubs = [] } = useGetPubsQuery(geocode ? geocode : skipToken)
+  const { data: pub, isSuccess } = useGetPubQuery(pubId ? pubId : skipToken)
 
   const handleChange = (search) => {
+    setInputValue(search)
     if (search.length > 1) {
       search = search.toLowerCase();
       const words = search.split(' ');
@@ -29,7 +35,7 @@ export default function SearchBar() {
         setTerm(search)
       }
     } else {
-      clearSearch();
+      setSearchResults([]);
     }
   };
 
@@ -37,9 +43,31 @@ export default function SearchBar() {
     setSearchResults(result)
   }, [term])
 
-  function clearSearch() {
-    setTerm('');
+  const getPub = (id) => {
+    if (!pubs.length) {
+      setPubId(id);
+      return;
+    }
+
+    const pub = pubs.find(obj => obj.id === id);
+
+    if (!pub) {
+      setPubId(id);
+    } else {
+      setAndClear(pub);
+    }
+  };
+
+  useEffect(() => {
+    if (isSuccess && pub) {
+      setAndClear(pub);
+    }
+  }, [isSuccess, pub]);
+
+  const setAndClear = (pub) => {
+    dispatch(setPub(pub));
     setSearchResults([]);
+    setInputValue('');
   }
 
   return (
@@ -48,12 +76,30 @@ export default function SearchBar() {
         <Input
           type="text"
           label="Search..."
-          className="w-full h-full bg-transparent border-none border-transparent focus:border-transparent text-lg ml-1 text-white"
+          className="w-full h-full bg-transparent border-none border-transparent focus:border-transparent text-lg ml-1 text-off_white"
           icon={<FaSearch className="text-black" />}
+          value={inputValue}
           onChange={(e) => handleChange(e.target.value)}
         />
       </div>
-      <SearchResults searchResults={searchResults} clearSearch={clearSearch} />
+
+
+      {searchResults != [] &&
+        <div
+          className="bg-gray-900 w-1/2 rounded-lg px-4 py-0 flex shadow-md items-center absolute top-4 left-1/2 flex-col mt-12 max-h-80 overflow-y-auto transform -translate-x-1/2 z-50 text-gray-300 scrollbar-thin scrollbar-thumb-black scrollbar-thumb-rounded-full"
+        >
+          {searchResults.map((rs) => (
+            <div
+              className="rounded-md h-10 cursor-pointer items-center flex hover:bg-gray-800 px-2 py-2 w-full mt-1"
+              onClick={() => getPub(rs.id)}
+              key={rs.id}
+            >
+              {correctEncoding(rs.name)}
+            </div>
+          ))}
+        </div>
+      }
+
     </>
   );
 }
---
+++ src/utils/stopWords.js
@@ -0,0 +1,7 @@
+const stopWords = [
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for', 'if', 'in', 'into', 'is', 'it', 'no',
+    'not', 'of', 'on', 'or', 'such', 'that', 'the', 'their', 'then', 'there', 'these', 'they', 'this',
+    'to', 'was', 'will', 'with'
+];
+
+export default stopWords;
