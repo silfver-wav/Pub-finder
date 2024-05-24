99ad4ccf8a96ef514a2b25e5b19d63902a7e9b9d
Linus Silfver <linus.silfver@gmail.com>
2024-05-25 00:05:53 +0200

    fixed sideBar.jsx and map searchedPub

--- src/sideBar/sideBar.jsx
+++ src/sideBar/sideBar.jsx
@@ -14,15 +14,6 @@ export default function SideBar() {
 
   const { data: visitedPubs, refetch } = useGetVisitedPubsQuery(getVisitedPubs ? user : skipToken)
 
-  function isEmpty(obj) {
-    for (const prop in obj) {
-      if (Object.hasOwn(obj, prop)) {
-        return false;
-      }
-    }
-    return true;
-  }
-
   useEffect(() => {
     // If user is logged in get VisitedPubs
     if (user) {
@@ -39,32 +30,31 @@ export default function SideBar() {
 
   return (
     <div className="bg-black rounded-xl p-2 absolute right-2 z-40 snap-y scrollbar-thin overflow-y-auto sm:w-50 md:w-72 lg:w-96 bottom-12 top-20 scroll-snap-type-y-mandatory" style={{ scrollPaddingTop: '10px' }}>
-      {!isEmpty(searchedPub) && (
-        <>
-          <div className="snap-center">
-            <BarTab
-              key={searchedPub.id}
-              pub={searchedPub}
-              user={user}
-              visited={visited(searchedPub)}
-              refetch={refetch}
-            />
-            <hr />
-          </div>
-        </>
+      {Object.keys(searchedPub).length != 0 && (
+        <div className="snap-center">
+          <BarTab
+            key={searchedPub.id}
+            pub={searchedPub}
+            user={user}
+            visited={visited(searchedPub)}
+            refetch={refetch}
+            isSearchedPub={true}
+          />
+        </div>
       )}
       {pubs.map((pub) => (
         <div className="snap-center" key={pub.id}>
-          <BarTab
-            pub={pub}
-            user={user}
-            visited={visited(pub)}
-            refetch={refetch}
-          />
+          {searchedPub.id != pub.id &&
+            <BarTab
+              pub={pub}
+              user={user}
+              visited={visited(pub)}
+              refetch={refetch}
+              isSearchedPub={false}
+            />
+          }
         </div>
       ))}
     </div>
-
-
   );
 }
